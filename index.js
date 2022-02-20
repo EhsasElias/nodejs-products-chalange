286f2f4bb15abd9d54e85d2b4c7c3c05c31576ca
Ehsas <ehsas.1998000@gmail.com>
2022-02-20 09:30:13 +0300

    thierd commit

--- index.js
+++ index.js
@@ -90,113 +90,90 @@ res.json(response);
 
 });
 
-// app.get('/products/:search?', async (req, res) => {
+app.get('/products/:search?', async (req, res) => {
 
-//     const usersearch = req.params.search;
-
-
-//     if (!usersearch) {
-//         const url = 'https://dummyjson.com/products/';
-
-//         const options = {
-//             "method": "GET"
-//         };
-//         const response = await fetch(url, options)
-//             .then(res => res.json())
-//             .catch(e => {
-//                 console.error({
-//                     "message": "oh opps",
-//                     error: e,
-//                 });
-//             });
-//         console.log("response:", response);
-//         res.json(response);
-//     }
-
-//     else {
-//         const url = 'https://dummyjson.com/products/' + usersearch;
-
-//         const options = {
-//             "method": "GET"
-//         };
-//         const response = await fetch(url, options)
-//             .then(res => res.json())
-//             .catch(e => {
-//                 console.error({
-//                     "message": "oh opps",
-//                     error: e,
-//                 });
-//             });
-//         console.log("response:", response);
-//         res.json(response);
-//     }
+    const usersearch = req.query.search;
 
 
-// });
+    if (!usersearch) {
+        const url = 'https://dummyjson.com/products/';
+
+        const options = {
+            "method": "GET"
+        };
+        const response = await fetch(url, options)
+            .then(res => res.json())
+            .catch(e => {
+                console.error({
+                    "message": "oh opps",
+                    error: e,
+                });
+            });
+        console.log("response:", response);
+        res.json(response);
+    }
+
+    else {
+        const url = 'https://dummyjson.com/products/' + usersearch;
+
+        const options = {
+            "method": "GET"
+        };
+        const response = await fetch(url, options)
+            .then(res => res.json())
+            .catch(e => {
+                console.error({
+                    "message": "oh opps",
+                    error: e,
+                });
+            });
+        console.log("response:", response);
+        res.json(response);
+    }
 
 
+});
 
+app.get('/products/categories', async (req, res) => {
+     const url = 'https://dummyjson.com/products/categories';
 
+        const options = {
+            "method": "GET"
+        };
+        const response = await fetch(url, options)
+            .then(res => res.json())
+            .catch(e => {
+                console.error({
+                    "message": "oh opps",
+                    error: e,
+                });
+            });
+        console.log("response:", response);
+        res.json(response);
+    
+});
 
+app.get('/products/category/:cateName', async (req, res) => {
+    const cate = req.params.cateName;
+    const url = 'https://dummyjson.com/products/category/'+cate;
 
-
-app.post('/api/courses', (req, res) => {
-    const { error } = validating(req.body);
-
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    return res.send(course);
+       const options = {
+           "method": "GET"
+       };
+       const response = await fetch(url, options)
+           .then(res => res.json())
+           .catch(e => {
+               console.error({
+                   "message": "oh opps",
+                   error: e,
+               });
+           });
+       console.log("response:", response);
+       res.json(response);
+   
 });
 
 
 
-app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The course with this id is not found');
-
-    const { error } = validating(req.body);
-
-    if (error) return res.status(400).send(error.details[0].message);
-
-
-
-    course.name = req.body.name;
-    return res.send(course);
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The course with this id is not found');
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    return res.send(course);
-
-});
-
-// app.get('/api/courses/:year/:month', (req ,res) => {
-//     res.send(req.query); or res.send(req.params);
-//     });
-// const server = http.createServer((req, res) => {
-//     if(req.url === '/'){
-//         res.write('Hello');
-//         res.end();
-//     }
-//     if(req.url === '/api/courses') {
-//         res.write(JSON.stringify[1, 2, 3]);
-//         res.end();
-//     }
-// });
-function validating(course) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    return Joi.validate(course, schema);
-}
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(port));
